e28238bd9a9cb8e0f1cabbf6b771e0165fd60b6d
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-07 14:59:27 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import Logo from "./assets/transparentLogoIcon.png";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="header"></div>
+      <div className="headerWrapper">
+        <Container fluid className="headerContainer">
+          <Row className="headerRow">
+            <Col className="headerMVCLogo">
+              <img className="headerImgMVCLogo" src={Logo} alt="MVC Logo" />
+            </Col>
+            <Col className="headerPhoneNumber"><h5>(404) 919-5150</h5></Col>
+            <Col className="headerLocationInfo"><h5>Location</h5></Col>
+            <Col className="headerContactInfo"><h5>Contact</h5></Col>
+            <Col className="headerMenuOptions"><h5>Menu</h5></Col>
+          </Row>
+        </Container>
+      </div>
     </>
   );
 }

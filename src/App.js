6884a3dbc9d6295a87e7c66789096d57ee965cf8
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-07 13:40:37 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from "react";
-// import logo from "./logo.svg";
 import Logo from "./assets/transparentLogoIcon.png";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={Logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <img className="MVCHeaderLogo" src={Logo} alt="MVC Logo" />
+      </div>
+    </>
   );
 }
 

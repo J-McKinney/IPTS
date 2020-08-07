83eae624ec0ec115d21b49c3171cb84accca0e45
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-07 17:26:45 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -8,46 +8,58 @@ import "./App.css";
 function App() {
   return (
     <>
-      {/* HEADER */}
-      <div className="headerWrapper">
-        <Container fluid className="headerContainer">
-          <Row className="headerRow">
-            <div id="headerMVCLogoCol" className="col-md-6">
-              <img className="headerMVCLogo" src={Logo} alt="MVC Logo" />
-            </div>
-            <div id="nestedCol" className="col-md-6">
-              <Row id="nestedRow" className="nestedRow">
-                <div id="headerMVCPhoneNumber" className="col-md-3">
-                  <h5>(404) 919-5150</h5>
-                </div>
-                <div id="headerMVCLocationInfo" className="col-md-3">
-                  <h5>LOCATION</h5>
-                </div>
-                <div id="headerMVCContactInfo" className="col-md-3">
-                  <h5>CONTACT</h5>
-                </div>
-                <div id="headerMVCMenu" className="col-md-3">
-                  <Row className="nestedMenuRow">
-                    <div id="menuBarIcon" className="col-md-3">
-                      <div className="bar1" />
-                      <div className="bar2" />
-                      <div className="bar3" />
-                    </div>
-                    <div className="col-md-9">
-                      <h5 className="headerh5Menu">MENU</h5>
-                    </div>
-                  </Row>
-                </div>
-              </Row>
-            </div>
-          </Row>
-        </Container>
-      </div>
-      {/* HEADER */}
+      <div className="SiteWrapper">
+        {/* HEADER */}
+        <div className="headerWrapper">
+          <Container fluid className="headerContainer">
+            <Row className="headerRow">
+              <div id="headerMVCLogoCol" className="col-md-6">
+                <img className="headerMVCLogo" src={Logo} alt="MVC Logo" />
+              </div>
+              <div id="nestedCol" className="col-md-6">
+                <Row id="nestedRow" className="nestedRow">
+                  <div id="headerMVCPhoneNumber" className="col-md-3">
+                    <h5>(404) 919-5150</h5>
+                  </div>
+                  <div id="headerMVCLocationInfo" className="col-md-3">
+                    <h5>LOCATION</h5>
+                  </div>
+                  <div id="headerMVCContactInfo" className="col-md-3">
+                    <h5>CONTACT</h5>
+                  </div>
+                  <div id="headerMVCMenu" className="col-md-3">
+                    <Row className="nestedMenuRow">
+                      <div id="menuBarIcon" className="col-md-3">
+                        <div className="bar1" />
+                        <div className="bar2" />
+                        <div className="bar3" />
+                      </div>
+                      <div className="col-md-9">
+                        <h5 className="headerh5Menu">MENU</h5>
+                      </div>
+                    </Row>
+                  </div>
+                </Row>
+              </div>
+            </Row>
+          </Container>
+        </div>
+        {/* HEADER */}
 
-      {/* MAIN CONTENT */}
-      <div className="bodyWrapper"></div>
-      {/* MAIN CONTENT */}
+        {/* MAIN CONTENT */}
+        <div className="mainContentWrapper">
+          <Container>
+            <video
+              autoPlay={true}
+              loop={true}
+              className="MVCVideo"
+              src={`https://d1l9wtg77iuzz5.cloudfront.net/assets/5637/236873/original.mp4?1530551954`}
+              alt="MVC Video"
+            ></video>
+          </Container>
+        </div>
+        {/* MAIN CONTENT */}
+      </div>
     </>
   );
 }

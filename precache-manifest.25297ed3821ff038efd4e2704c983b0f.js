afb82f15b8db9f872ce7f1ac9bbfc47fa115b84d
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-07 23:44:19 -0400

    Updates

---
+++ precache-manifest.25297ed3821ff038efd4e2704c983b0f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52fc81cdfddb4908c4c00a0dbb50fab0",
+    "url": "/IPTS/index.html"
+  },
+  {
+    "revision": "c94505c711139838f845",
+    "url": "/IPTS/static/css/main.a85f3813.chunk.css"
+  },
+  {
+    "revision": "006311e3705e16d3d366",
+    "url": "/IPTS/static/js/2.29dd2285.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/IPTS/static/js/2.29dd2285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c94505c711139838f845",
+    "url": "/IPTS/static/js/main.ae76838f.chunk.js"
+  },
+  {
+    "revision": "6bd77cc4f7fd662b4517",
+    "url": "/IPTS/static/js/runtime-main.27d9cd14.js"
+  },
+  {
+    "revision": "10a320a504697d4990670a841b567021",
+    "url": "/IPTS/static/media/dogCarWindow.10a320a5.jpg"
+  },
+  {
+    "revision": "7a816171ff444ef6ef1fa34e3b4acba7",
+    "url": "/IPTS/static/media/dogInCrate.7a816171.jpg"
+  },
+  {
+    "revision": "e7441c12d2a378e491662d309e21b664",
+    "url": "/IPTS/static/media/transparentLogoIcon.e7441c12.png"
+  }
+]);

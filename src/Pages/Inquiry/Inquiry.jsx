889a74d702aa0a706fcf97f925d81e5064243348
Jesse McKinney <jessemckinney513@gmail.com>
2020-08-20 14:36:01 -0400

    yay

--- src/Pages/Inquiry/Inquiry.jsx
+++ src/Pages/Inquiry/Inquiry.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
-// import Row from "react-bootstrap/Row";
+import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 // Form Control
@@ -35,15 +35,43 @@ class Inquiry extends Component {
     petArrivalCity: "",
     petArrivalState: "",
     petArrivalCountry: "",
+    additionalComments: "",
   };
 
+  //
+  componentWillMount() {
+    console.log("WillMount, ownerFirstName: ", this.state.ownerFirstName);
+    console.log("WillMount, ownerLastName: ", this.state.ownerLastName);
+    console.log("WillMount, petType: ", this.state.petType);
+  }
   componentDidMount() {
-    console.log("componentDidMount: ");
+    console.log("DidMount, ownerFirstName: ", this.state.ownerFirstName);
+    console.log("DidMount, ownerLastName: ", this.state.ownerLastName);
+    console.log("DidMount, petType: ", this.state.petType);
   }
-
+  componentWillUpdate() {
+    console.log("WillUpdate, ownerFirstName: ", this.state.ownerFirstName);
+    console.log("WillUpdate, ownerLastName: ", this.state.ownerLastName);
+    console.log("WillUpdate, petType: ", this.state.petType);
+  }
   componentDidUpdate() {
-    console.log("componentDidUpdate: ");
+    console.log("DidUpdate, ownerFirstName: ", this.state.ownerFirstName);
+    console.log("DidUpdate, ownerLastName: ", this.state.ownerLastName);
+    console.log("DidUpdate, petType: ", this.state.petType);
   }
+  componentWillUnmount() {
+    console.log("WillUnmount, ownerFirstName: ", this.state.ownerFirstName);
+    console.log("WillUnmount, ownerLastName: ", this.state.ownerLastName);
+    console.log("WillUnmount, petType: ", this.state.petType);
+  }
+  //
+
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
+  };
 
   onSubmit = (e) => {
     e.preventDefault();
@@ -59,47 +87,73 @@ class Inquiry extends Component {
           <Container className={Style.ownerInfoContainer}>
             <Form>
               <br />
-              {/* First Name */}
               <Form.Row>
+                {/* First Name */}
                 <Col>
                   <Form.Label>First Name</Form.Label>
-                  <Form.Control placeholder="First name" />
+                  <Form.Control
+                    value={this.state.ownerFirstName}
+                    name="ownerFirstName"
+                    onChange={this.handleInputChange}
+                    type="text"
+                    placeholder="First name"
+                  />
                 </Col>
+                {/* First Name */}
                 {/* Last Name */}
                 <Col>
                   <Form.Label>Last Name</Form.Label>
-                  <Form.Control placeholder="Last name" />
+                  <Form.Control
+                    value={this.state.ownerLastName}
+                    name="ownerLastName"
+                    onChange={this.handleInputChange}
+                    type="text"
+                    placeholder="Last name"
+                  />
                 </Col>
+                {/* Last Name */}
               </Form.Row>
               <br />
-              {/* Type Of Animal */}
-              <Form.Row>
-                <Form.Label>Type Of Animal</Form.Label>
-              </Form.Row>
-              <Form.Row>
-                {/* Cat??? */}
-                <Col>
-                  <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Cat: Meow" />
-                  </Form.Group>
-                </Col>
-                {/* Dog??? */}
-                <Col>
-                  <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Dog: Woof" />
-                  </Form.Group>
-                </Col>
-              </Form.Row>
+              {/* Type of Animal Radio Buttons */}
+              <fieldset>
+                <Form.Group as={Row}>
+                  <Form.Label as="legend" column sm={4}>
+                    Type Of Animal:
+                  </Form.Label>
+                  <Col sm={4}>
+                    <Form.Check
+                      value={this.state.petType}
+                      type="radio"
+                      label="Dog Woof"
+                      name="petType"
+                      id="formHorizontalRadios1"
+                    />
+                  </Col>
+                  <Col sm={4}>
+                    <Form.Check
+                      value={this.state.petType}
+                      type="radio"
+                      label="Cat Meow"
+                      name="petType"
+                      id="formHorizontalRadios2"
+                    />
+                  </Col>
+                </Form.Group>
+              </fieldset>
+              {/* Type of Animal Radio Buttons */}
               <br />
               {/* Text Area */}
               <Form.Group controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Additional Comments</Form.Label>
                 <Form.Control as="textarea" rows="5" maxLength="600" />
               </Form.Group>
+              {/* Text Area */}
               <br />
+              {/* Submit Button */}
               <Button onClick={this.onSubmit} variant="primary" type="submit">
                 Submit
               </Button>
+              {/* Submit Button */}
               <br />
               {/* End Of Form */}
             </Form>
